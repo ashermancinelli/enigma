2ea3d13b94e8c4ef309bee072a5a9385f4e2b26a
ashermancinelli <ashermancinelli@gmail.com>
2018-01-12 15:51:07 -0800

    developed data structures implementations

--- enigma/dataStructures.py
+++ enigma/dataStructures.py
@@ -8,23 +8,72 @@
 #
 #
 
-letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# These are the default dictionaries for each rotor, and can be modified 
+# by shuffling or adding new letters into the subsequent loops.
+_rotorDicts = [
+    { char.lower(): char.lower() for char in PlugBoard.letters },
+    { char.lower(): char.lower() for char in PlugBoard.letters },
+    { char.lower(): char.lower() for char in PlugBoard.letters },
+    { char.lower(): char.lower() for char in PlugBoard.letters },
+    { char.lower(): char.lower() for char in PlugBoard.letters },
+    { char.lower(): char.lower() for char in PlugBoard.letters },
+    { char.lower(): char.lower() for char in PlugBoard.letters },
+    { char.lower(): char.lower() for char in PlugBoard.letters },
+]
+
+_shuffleKeys = [
+    [[],[],[],[],[],[],[],[],[]],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+]
+
+for rdIndex in len(_rotorDicts):
+    rd[_shuffleKeys[rdIndex][0]]] = _shuffleKeys[rdIndex][1]
+    rd[_shuffleKeys[rdIndex][1]]] = _shuffleKeys[rdIndex][0]
 
 
-class enigma:
+class EnigmaMachine:
 
-    __init__(self, activeRotors, plugBoard):
+    rotors = []
+
+    __init__(self, activeRotorIndeces, plugBoardPairs):
+
+        # rotor index indicates which rotor will be in the enigma machine 
+        # from right to left, as that is the order the signal would travel through
+        # the actual enigma machine. An example of initializing an EnigmaMachine object
+        # would be:
+        #   em = EnigmaMachine([1, 5, 2], [['a', 't'], ['s', 'v'], ['l', 'k']])
+        # In the example above, the rotors would be positioned with rotor 1 rightmost,
+        # rotor 5 in the middle, and rotor 2 leftmost in a real enigma machine. The 
+        # letters 'a' and 't' would be mapped to each other in the plugboard, as well 
+        # as 's' and 'v', etc etc.
+        for r in activeRotorIndeces:
+            self.rotors.append(Rotor(r))
         
-        for rotor in activeRotors:
-            self.rotors.append(rotor)
 
-class rotor:
-class plugBoard:
+class Rotor:
 
-    plugBoardMapping = { char: char for char in letters }  
+    _rotorIndex = 0
+
+    __init__(self, rotorIndex):
+        if rotorIndex < 1 or rotorIndex > 8: raise IndexError
+        self._rotorIndex = rotorIndex
+
+class PlugBoard:
+
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.lower()
+
+    # the plug board is originally set up as a mapping from each letter to itself.
+    plugBoardMapping = { char.lower(): char.lower() for char in PlugBoard.letters }  
     
-    __init__(self, plugBoardList):
-        for pair in plugBoardList:
+    __init__(self, plugBoardPairs):
+        for pair in plugBoardPairs:
             plugBoardMapping[pair[0]] = pair[1]
             plugBoardMapping[pair[1]] = pair[0]
     
