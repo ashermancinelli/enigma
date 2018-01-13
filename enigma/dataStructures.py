87e29e3f86bc21d83872774b5e413d6f80c78feb
ashermancinelli <ashermancinelli@gmail.com>
2018-01-12 22:47:02 -0800

    further implemented data structures

--- enigma/dataStructures.py
+++ enigma/dataStructures.py
@@ -11,6 +11,7 @@
 
 # These are the default dictionaries for each rotor, and can be modified 
 # by shuffling or adding new letters into the subsequent loops.
+##################################################################################
 letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.lower()
 
 _rotorDicts = [
@@ -21,10 +22,17 @@ _rotorDicts = [
 ]
 
 _shuffleKeys = [
-    [['j','p'],['y','l'],['f','v'],['n','u'],['k','g'],['w','h'],['o','d'],['e','c'],['b','q'],['t','z'],['i','a'],['r','m'],['x','s']],
-    [['o','q'],['y','m'],['a','g'],['i','j'],['s','r'],['v','k'],['z','u'],['b','t'],['c','e'],['w','h'],['n','l'],['f','p'],['d','x']],
-    [['q','x'],['z','t'],['y','o'],['s','r'],['i','d'],['h','w'],['f','m'],['c','k'],['v','g'],['b','e'],['u','j'],['l','p'],['n','a']],
-    [['q','h'],['l','t'],['v','n'],['p','b'],['k','a'],['d','z'],['r','m'],['f','y'],['e','w'],['u','o'],['i','x'],['g','j'],['s','c']]
+    [['j','p'],['y','l'],['f','v'],['n','u'],['k','g'],['w','h'],
+     ['o','d'],['e','c'],['b','q'],['t','z'],['i','a'],['r','m'],['x','s']],
+
+    [['o','q'],['y','m'],['a','g'],['i','j'],['s','r'],['v','k'],
+     ['z','u'],['b','t'],['c','e'],['w','h'],['n','l'],['f','p'],['d','x']],
+
+    [['q','x'],['z','t'],['y','o'],['s','r'],['i','d'],['h','w'],
+     ['f','m'],['c','k'],['v','g'],['b','e'],['u','j'],['l','p'],['n','a']],
+
+    [['q','h'],['l','t'],['v','n'],['p','b'],['k','a'],['d','z'],
+     ['r','m'],['f','y'],['e','w'],['u','o'],['i','x'],['g','j'],['s','c']]
 ]
 
 for index in range(4):
@@ -32,6 +40,25 @@ for index in range(4):
         _rotorDicts[index][pair[0]] = pair[1]
         _rotorDicts[index][pair[1]] = pair[0]
 
+##################################################################################
+
+
+
+
+
+
+# this is the dictionary for the reflector, a special instance 
+# of the rotor class since they operate almost the same
+
+##################################################################################
+_reflectorDict = { char.lower(): char.lower() for char in letters }
+_reflectorShuffleKeys = [['m', 'q'],['v','u'],['r','i'],['d','b'],['x','p'],['y','l'],
+                         ['z','f'],['j','t'],['h','e'],['o','a'],['w','s'],['n','c'],['k','g']]
+
+for pair in _reflectorShuffleKeys:
+    _reflectorDict[pair[0]] = pair[1]
+    _reflectorDict[pair[1]] = pair[0]
+##################################################################################
 
 
 class EnigmaMachine:
@@ -51,15 +78,67 @@ class EnigmaMachine:
         # as 's' and 'v', etc etc.
         for r in activeRotorIndeces:
             self.rotors.append(Rotor(r))
+
+        # this is a special case of the Rotor class, where the reflector is 
+        # defined. This acts essentially like a normal rotor would, but has its own 
+        # dictionary defined. 
+        reflector = Rotor(-1)
+
+        pb = PlugBoard()
+
+    def evaluate(char):
+
+        l = pb.evaluate(char)
+        
+        for r in rotors:
+            l = r.evaluate(l)
+        
+        l = reflector.evaluate(l)
+        
+        numRotors = len(rotors)
+        
+        i = numRotors
+        
+        while i >= 0:
+            l = rotors[i].evaluate(l)
+            i -= 1
+        
         
 
 class Rotor:
 
     _rotorIndex = 0
+    _rotorDict = {}
+
 
     __init__(self, rotorIndex):
-        if rotorIndex < 1 or rotorIndex > 8: raise IndexError
+        if rotorIndex < 1 or rotorIndex > 8: 
+            if rotorIndex == -1:
+                _rotorDict = _reflectorDict
+            else:   raise IndexError
+
         self._rotorIndex = rotorIndex
+        self._rotorDict = _rotorDicts[rotorIndex]
+
+    def rotate():
+        dictList = []
+        newDictList = []
+
+        # dictionaries are not iterable with an index, so we convert the dictionary 
+        # to a list first, copy over the adjusted list, and then fill the dict with the 
+        # appropriate offset values.
+        for k, v in _rotorDict:
+            dictList.append([k, v])
+        
+        for index in len(dictList):
+            newDictList[index] = dictList[(index+1)%26]
+
+        _rotorDict = {  }
+
+
+    def evaluate(char):
+        return _rotorDict[char]
+        rotate()
 
 class PlugBoard:
 
@@ -72,6 +151,9 @@ class PlugBoard:
         for pair in plugBoardPairs:
             plugBoardMapping[pair[0]] = pair[1]
             plugBoardMapping[pair[1]] = pair[0]
+
+    def evaluate(char):
+        return plugBoardMapping[char.lower()]
     
     
 
