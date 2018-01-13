eb69294aa5fe09ba026a36a8df822c116f204a6e
ashermancinelli <ashermancinelli@gmail.com>
2018-01-13 13:09:18 -0800

    Finished new implementation of rotors and enigmamachine

--- enigma/CLIController.py
+++ enigma/CLIController.py
@@ -1,8 +1,9 @@
 from EnigmaMachine import *
 
 if __name__ == '__main__':
-    em = EnigmaMachine([1,2,3],[['t', 's'], ['z', 'a'], ['e', 'n']])
-    pt = 'a'
+
+    em = EnigmaMachine([1,2,3],[['t', 's'], ['z', 'a'], ['e', 'n']], ['a','a','a'])
+    pt = input("enter the plaintext to be encrypted\n>>>")
     ct = ''
     for ch in pt:
         ct += em.evaluate(ch)
--- enigma/EnigmaMachine.py
+++ enigma/EnigmaMachine.py
@@ -1,13 +1,15 @@
 
 from PlugBoard import *
-from Rotor import *
-
+# from Rotor import *
+from RotorByList import *
 
 class EnigmaMachine:
 
-    rotors = []
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.lower()
+    _rotors = [] 
+    _rotationsByRotor = []
 
-    def __init__(self, activeRotorIndeces, plugBoardPairs):
+    def __init__(self, activeRotorIndeces, plugBoardPairs, rotorStartingPositions):
 
         self.pb = PlugBoard(plugBoardPairs)
 
@@ -15,43 +17,57 @@ class EnigmaMachine:
         # from right to left, as that is the order the signal would travel through
         # the actual enigma machine. An example of initializing an EnigmaMachine object
         # would be:
-        #   em = EnigmaMachine([1, 5, 2], [['a', 't'], ['s', 'v'], ['l', 'k']])
+        #   em = EnigmaMachine([1, 5, 2], [['a', 't'], ['s', 'v'], ['l', 'k']], ['a','a','a'])
         # In the example above, the rotors would be positioned with rotor 1 rightmost,
         # rotor 5 in the middle, and rotor 2 leftmost in a real enigma machine. The 
         # letters 'a' and 't' would be mapped to each other in the plugboard, as well 
         # as 's' and 'v', etc etc.
-        r1 = Rotor(activeRotorIndeces[0])
-        r2 = Rotor(activeRotorIndeces[1])
-        r3 = Rotor(activeRotorIndeces[2])
-        self.rotors = [r1, r2, r3]
-        if len(activeRotorIndeces) > 3:
-            r4 = Rotor(activeRotorIndeces[3])
-            self.rotors.append(r4)
-        if len(activeRotorIndeces) > 4:
-            r5 = Rotor(activeRotorIndeces[4])
-            self.rotors.append(r5)
+        for ari in activeRotorIndeces:
+            self._rotors.append(Rotor(ari))
+
+        for r in self._rotors:
+            self._rotationsByRotor.append(0)        
+
+        self._totalRotations = 0
 
         # this is a special case of the Rotor class, where the reflector is 
         # defined. This acts essentially like a normal rotor would, but has its own 
         # dictionary defined. 
         self.reflector = Rotor(-1)
 
+        for index in range(len(self._rotors)):
+            for rotation in range(self.letters.index(rotorStartingPositions[index])):
+                self._rotors[index].rotate()
+
+
         
+    def rotationHandler(self):
+        self._totalRotations += 1
+        
+        self._rotors[0].rotate()
+        self._rotationsByRotor[0] += 1
+
+        for index in range(len(self._rotors) - 1):
+            while ( self._rotationsByRotor[index] // 26 ) > self._rotationsByRotor[ index + 1 ]:
+                self._rotors[ index + 1 ].rotate()
+
 
 
     def evaluate(self, char):
 
         l = self.pb.evaluate(char)
+
+        self.rotationHandler()
         
-        for r in self.rotors:
+        for r in self._rotors:
             l = r.evaluate(l)
         
         l = self.reflector.evaluate(l)
         
-        i = len(self.rotors) - 1
+        i = len(self._rotors) - 1
         
         while i >= 0:
-            l = self.rotors[i].evaluate(l)
+            l = self._rotors[i].evaluate(l, reverse=True)
             i -= 1
 
         return self.pb.evaluate(l)
